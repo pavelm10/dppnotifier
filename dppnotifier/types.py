56a7e95601c1a4341a45759648662247af36765c
Marek Pavelka <marek.pavelka@datamole.cz>
2022-08-13 13:22:32 +0200

    types: add to_dict conversion
    Needed for correct datetime serialization

--- dppnotifier/types.py
+++ dppnotifier/types.py
@@ -1,6 +1,6 @@
-from dataclasses import dataclass
+from dataclasses import asdict, dataclass
 from datetime import datetime
-from typing import List, Optional
+from typing import Dict, List, Optional
 
 
 @dataclass
@@ -11,3 +11,17 @@ class TrafficEvent:
     lines: List[str]
     message: str
     event_id: str
+
+    def to_dict(self) -> Dict:
+        start_date = self.start_date
+        if start_date is not None:
+            start_date = datetime.strftime(start_date, '%Y%m%dT%H%M')
+
+        end_date = self.end_date
+        if end_date is not None:
+            end_date = datetime.strftime(end_date, '%Y%m%dT%H%M')
+
+        self_dict = asdict(self)
+        self_dict['start_date'] = start_date
+        self_dict['end_date'] = end_date
+        return self_dict
