4f8ea98c6b713ce35e001a4850ab79e9ea7aa6cf
Marek Pavelka <marek.pavelka@datamole.cz>
2022-08-13 18:14:25 +0200

    add cli for adding recepient

---
+++ dppnotifier/add_user_cli.py
@@ -0,0 +1,29 @@
+from dppnotifier.db import DynamoDb
+from dppnotifier.types import Notifiers, Recepient
+
+
+def main():
+    import argparse
+
+    argp = argparse.ArgumentParser()
+    argp.add_argument('-u', '--user', required=True, help='user name')
+    argp.add_argument(
+        '-r',
+        '--uri',
+        required=True,
+        help='email address, whatsapp id, etc. of the user',
+    )
+    argp.add_argument('-n', '--notifier', required=True, help='notifier type')
+
+    pargs = argp.parse_args()
+    user = pargs.user
+    uri = pargs.uri
+    notifier = pargs.notifier
+
+    recepient = Recepient(notifier=Notifiers(notifier), uri=uri, user=user)
+    db_client = DynamoDb(table_name='dpp-notifier-recepients')
+    db_client.add_recepient(recepient)
+
+
+if __name__ == '__main__':
+    main()
