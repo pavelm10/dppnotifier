7eb58eaebd8a8997b588a6329dee8a942b19e411
Marek Pavelka <marek.pavelka@datamole.cz>
2022-08-13 13:24:19 +0200

    db: add encoding

--- dppnotifier/db.py
+++ dppnotifier/db.py
@@ -4,9 +4,10 @@
 
 import json
 from abc import ABC, abstractmethod
+from base64 import encode
 from dataclasses import asdict
 from pathlib import Path
-from typing import Optional
+from typing import List, Optional
 
 from dppnotifier.types import TrafficEvent
 
@@ -24,19 +25,22 @@ class BaseDb(ABC):
 class JsonDb(BaseDb):
     def __init__(self, file_path: Path) -> None:
         self._file_path = file_path
-        self.db = self._load()
+        self._load()
 
     def _load(self):
         if not self._file_path.exists():
             self.db = {}
             return
 
-        with open(self._file_path, 'r') as file:
+        with open(self._file_path, 'r', encoding='utf-8') as file:
             self.db = json.load(file)
 
     def _dump(self):
-        with open(self._file_path, 'w') as file:
-            json.dump(self.db, file)
+        if len(self.db) == 0:
+            return
+
+        with open(self._file_path, 'w', encoding='utf-8') as file:
+            json.dump(self.db, file, ensure_ascii=False)
 
     def find_by_id(self, event_id: str) -> Optional[TrafficEvent]:
         event = self.db.get(event_id)
@@ -45,5 +49,5 @@ class JsonDb(BaseDb):
         return event
 
     def upsert_event(self, event: TrafficEvent):
-        self.db[event.event_id] = asdict(event)
+        self.db[event.event_id] = event.to_dict()
         self._dump()
