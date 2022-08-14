48327454da2311121a3cec6c2ee7b2ae6bb05251
Marek Pavelka <marek.pavelka@datamole.cz>
2022-08-14 16:32:12 +0200

    app: refactor to OOP, filter by lines

--- dppnotifier/app.py
+++ dppnotifier/app.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+from typing import Set
 
 from dppnotifier.db import JsonDb
 from dppnotifier.log import init_logger
@@ -9,33 +10,62 @@ from dppnotifier.types import Notifiers, Recepient
 _LOGGER = init_logger(__name__)
 
 
-def notify(issue: TrafficEvent):
-    notifiers = [
-        AwsSesNotifier(),
-        WhatsAppNotifier(),
-        LogNotifier(),
-    ]
-    aws_recepients = (
-        Recepient(
-            notifier=Notifiers.AWS_SES,
-            uri='marek.pavelka12@gmail.com',
-            user='Marek Pavelka',
-        ),
-    )
-    whatsapp_recepients = ()
-    log_recepients = ()
-    recepients = [aws_recepients, whatsapp_recepients, log_recepients]
-    for notifier, recepient_list in zip(notifiers, recepients):
-        notifier.notify([issue], recepient_list)
+class DppNotificationApp:
+    def __init__(self) -> None:
+        self._aws_notifier = AwsSesNotifier()
+        self._whatsapp_notifer = WhatsAppNotifier()
+        self._log_notifier = LogNotifier()
+        self._get_subscribers()
+        self.db = JsonDb(file_path=Path('data/events.json'))
+
+    def _get_subscribers(self):
+        self._aws_recepients = (
+            Recepient(
+                notifier=Notifiers.AWS_SES,
+                uri='marek.pavelka12@gmail.com',
+                user='Marek Pavelka',
+            ),
+        )
+        self._whatsapp_recepients = ()
+        self._log_recepients = ()
+
+    def _notify(self, event: TrafficEvent):
+        aws_subs = self._filter_subscriber(event, self._aws_recepients)
+        whatsapp_subs = self._filter_subscriber(
+            event, self._whatsapp_recepients
+        )
+
+        self._aws_notifier.notify([event], tuple(aws_subs))
+        self._whatsapp_notifer.notify([event], tuple(whatsapp_subs))
+        self._log_notifier.notify([event])
+
+    def __call__(self, *args, **kwds):
+        _LOGGER.info('Fetching current events')
+        for event in fetch_events():
+            try:
+                self.db.upsert_event(event)
+            except (ValueError, IndexError, KeyError) as exc:
+                _LOGGER.error(
+                    'Failed to upsert the event - notification skipped'
+                )
+                _LOGGER.error(exc)
+                continue
+
+            if event.active and event.event_id not in self.db.db.keys():
+                self._notify(event)
+
+    @staticmethod
+    def _filter_subscriber(event, subscribers) -> Set[Recepient]:
+        subs = []
+        for sub in subscribers:
+            if set(sub.lines).issubset(set(event.lines)):
+                subs.append(sub)
+        return set(subs)
 
 
 def main():
-    db = JsonDb(file_path=Path('data/events.json'))
-    _LOGGER.info('Fetching current events')
-    for event in fetch_events():
-        db.upsert_event(event)
-        if event.active and event.event_id not in db.db.keys():
-            notify(event)
+    app = DppNotificationApp()
+    app()
 
 
 if __name__ == '__main__':
