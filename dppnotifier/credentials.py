17084dc5a149f00e29f57e3e157dfcf0c4eae037
Marek Pavelka <marek.pavelka@datamole.cz>
2022-08-22 19:15:27 +0200

    add whatsapp credential class

---
+++ dppnotifier/credentials.py
@@ -0,0 +1,20 @@
+import json
+from dataclasses import dataclass
+from pathlib import Path
+
+
+@dataclass
+class WhatsAppCredential:
+    token: str
+    phone_id: str
+    account_id: str
+
+    @classmethod
+    def from_file(cls, credential_path: Path):
+        with credential_path.open('r', encoding='utf-8') as file:
+            data = json.load(file)
+            return cls(
+                token=data['token'],
+                phone_id=data['phone_id'],
+                account_id=data['account_id'],
+            )
