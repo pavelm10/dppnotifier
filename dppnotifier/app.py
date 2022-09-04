10c7259caac5711e84855f6eb29f0399434c1d22
Marek Pavelka <marek.pavelka@datamole.cz>
2022-09-04 20:23:13 +0200

    refactor app to simple functions

--- dppnotifier/app.py
+++ dppnotifier/app.py
@@ -10,81 +10,76 @@ from dppnotifier.types import NotifierSubscribers, Recepient
 _LOGGER = init_logger(__name__)
 
 
-class DppNotificationApp:
-    def __init__(self):
-        self.events_db = DynamoTrafficEventsDb(
-            table_name=os.getenv('EVENTS_TABLE', 'dpp-notifier-events')
+def build_notifiers(
+    subscribers_db: DynamoSubscribersDb,
+) -> List[NotifierSubscribers]:
+    possible_notifiers = (AwsSesNotifier, WhatsAppNotifier)
+    notifiers = []
+    for notifier_class in possible_notifiers:
+        subscribers = subscribers_db.get_recepients(
+            notifier_type=notifier_class.NOTIFIER_TYPE
         )
-        self.subs_db = DynamoSubscribersDb(
-            table_name=os.getenv(
-                'SUBSCRIBERS_TABLE', 'dpp-notifier-recepients'
-            )
-        )
-        self._notifiers = self._build_notifiers()
-
-    def _build_notifiers(self) -> List[NotifierSubscribers]:
-        possible_notifiers = (AwsSesNotifier, WhatsAppNotifier)
-        notifiers = []
-        for notifier_class in possible_notifiers:
-            subscribers = self.subs_db.get_recepients(
-                notifier_type=notifier_class.NOTIFIER_TYPE
-            )
-            if len(subscribers) > 0:
-                notifier = notifier_class()
-                if notifier.enabled:
-                    notifiers.append(
-                        NotifierSubscribers(
-                            notifier=notifier,
-                            subscribers=subscribers,
-                        )
+        if len(subscribers) > 0:
+            notifier = notifier_class()
+            if notifier.enabled:
+                notifiers.append(
+                    NotifierSubscribers(
+                        notifier=notifier,
+                        subscribers=subscribers,
                     )
-                    _LOGGER.info(
-                        '%s notifier registered', notifier.NOTIFIER_TYPE.value
-                    )
-
-        _LOGGER.info('Notifiers and subscribers set up')
-        return notifiers
-
-    def _notify(self, event: TrafficEvent):
-        for notifier_subscribers in self._notifiers:
-            subs = self._filter_subscriber(
-                event, notifier_subscribers.subscribers
-            )
-            if len(subs) > 0:
-                notifier_subscribers.notifier.notify(event, subs)
-
-    def _log_event(self, event: TrafficEvent):
-        _LOGGER.info('Started %s, URL: %s', event.start_date, event.url)
-
-    def __call__(self, *args, **kwds):
-        _LOGGER.info('Fetching current events')
-        for event in fetch_events():
-            self._log_event(event)
-            db_event = self.events_db.find_by_id(event.event_id)
-
-            try:
-                self.events_db.upsert_event(event)
-            except (ValueError, IndexError, KeyError) as exc:
-                _LOGGER.error(
-                    'Failed to upsert the event - notification skipped'
                 )
-                _LOGGER.error(exc)
-                continue
+                _LOGGER.info(
+                    '%s notifier registered', notifier.NOTIFIER_TYPE.value
+                )
 
-            if event.active and db_event is None:
-                self._notify(event)
+    _LOGGER.info('Notifiers and subscribers set up')
+    return notifiers
 
-    @staticmethod
-    def _filter_subscriber(event, subscribers) -> Tuple[Recepient]:
-        subs = []
-        for sub in subscribers:
-            if set(sub.lines).issubset(set(event.lines)):
-                subs.append(sub)
-        return tuple(subs)
+
+def notify(notifiers: List[NotifierSubscribers], event: TrafficEvent):
+    for notifier_subscribers in notifiers:
+        subs = filter_subscriber(event, notifier_subscribers.subscribers)
+        if len(subs) > 0:
+            notifier_subscribers.notifier.notify(event, subs)
+
+
+def log_event(event: TrafficEvent):
+    _LOGGER.info('Started %s, URL: %s', event.start_date, event.url)
+
+
+def filter_subscriber(
+    event: TrafficEvent, subscribers: List[Recepient]
+) -> Tuple[Recepient]:
+    subs = []
+    for sub in subscribers:
+        if set(sub.lines).issubset(set(event.lines)):
+            subs.append(sub)
+    return tuple(subs)
 
 
 def main():
-    DppNotificationApp()()
+    events_db = DynamoTrafficEventsDb(
+        table_name=os.getenv('EVENTS_TABLE', 'dpp-notifier-events')
+    )
+    subs_db = DynamoSubscribersDb(
+        table_name=os.getenv('SUBSCRIBERS_TABLE', 'dpp-notifier-recepients')
+    )
+    notifiers = build_notifiers(subs_db)
+
+    _LOGGER.info('Fetching current events')
+    for event in fetch_events():
+        log_event(event)
+        db_event = events_db.find_by_id(event.event_id)
+
+        try:
+            events_db.upsert_event(event)
+        except (ValueError, IndexError, KeyError) as exc:
+            _LOGGER.error('Failed to upsert the event - notification skipped')
+            _LOGGER.error(exc)
+            continue
+
+        if event.active and db_event is None:
+            notify(notifiers, event)
 
 
 if __name__ == '__main__':
