8aa38849203aa2c21f3468da595b1c2c86c823f3
Marek Pavelka <marek.pavelka@datamole.cz>
2022-09-27 07:59:37 +0200

    build-package: remove sudo

--- scripts/build-package.sh
+++ scripts/build-package.sh
@@ -13,7 +13,7 @@ rm -rf .venv
 pyenv local 3.9.10
 poetry install --no-dev
 cd .venv/lib/python3.9/site-packages
-sudo zip -r ../../../../$PACKAGE_NAME .
+zip -r ../../../../$PACKAGE_NAME .
 cd ../../../../
-sudo zip -g -r $PACKAGE_NAME dppnotifier/app
-sudo zip -g -r $PACKAGE_NAME secrets
+zip -g -r $PACKAGE_NAME dppnotifier/app
+zip -g -r $PACKAGE_NAME secrets
