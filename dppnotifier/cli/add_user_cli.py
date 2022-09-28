5f31961cc5fb92e6dff7924b2b46f321d60fafaa
Marek Pavelka <marek.pavelka@datamole.cz>
2022-09-28 18:03:13 +0200

    cli: add lines arg to add_user_cli.py

--- dppnotifier/cli/add_user_cli.py
+++ dppnotifier/cli/add_user_cli.py
@@ -21,16 +21,26 @@ def main():
     )
     argp.add_argument('-n', '--notifier', required=True, help='notifier type')
     argp.add_argument(
+        '-l', '--lines', required=False, help='list of lines, comma separated'
+    )
+    argp.add_argument(
         '-t', '--table', required=False, default='dpp-notifier-recepients'
     )
 
     pargs = argp.parse_args()
     user = pargs.user
     uri = pargs.uri
+    lines = pargs.lines
+    if lines is None:
+        lines = ()
+    else:
+        lines = lines.split(',')
     notifier = pargs.notifier
     table = pargs.table
 
-    subscriber = Subscriber(notifier=Notifiers(notifier), uri=uri, user=user)
+    subscriber = Subscriber(
+        notifier=Notifiers(notifier), uri=uri, lines=lines, user=user
+    )
     db_client = DynamoSubscribersDb(table_name=table)
     db_client.add_subscriber(subscriber)
     _LOGGER.info('Added subscriber %s', subscriber)
