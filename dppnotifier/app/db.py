a94849a62e309fda4a87d12f5640ae8d32b99271
Marek Pavelka <marek.pavelka@datamole.cz>
2022-09-28 16:56:19 +0200

    remove json db

--- dppnotifier/app/db.py
+++ dppnotifier/app/db.py
@@ -1,8 +1,6 @@
-import json
 import logging
 import os
 from abc import ABC, abstractmethod
-from pathlib import Path
 from typing import List, Optional
 
 import boto3
@@ -11,16 +9,6 @@ from boto3.dynamodb.conditions import Attr, Key
 from dppnotifier.app.types import Notifiers, Subscriber, TrafficEvent
 
 _LOGGER = logging.getLogger(__name__)
-
-
-class TrafficEventsDb(ABC):
-    @abstractmethod
-    def find_by_id(self, event_id: str) -> Optional[TrafficEvent]:
-        raise NotImplementedError
-
-    @abstractmethod
-    def upsert_event(self, event: TrafficEvent):
-        raise NotImplementedError
 
 
 class SubscribersDb(ABC):
@@ -55,39 +43,7 @@ class DynamoDb:
         self._table = self._client.Table(table_name)
 
 
-class JsonTrafficEventsDb(TrafficEventsDb):
-    def __init__(self, file_path: Path) -> None:
-        self._file_path = file_path
-        self._load()
-        _LOGGER.info('DB loaded')
-
-    def _load(self):
-        if not self._file_path.exists():
-            self.db = {}
-            return
-
-        with open(self._file_path, 'r', encoding='utf-8') as file:
-            self.db = json.load(file)
-
-    def _dump(self):
-        if len(self.db) == 0:
-            return
-
-        with open(self._file_path, 'w', encoding='utf-8') as file:
-            json.dump(self.db, file, ensure_ascii=False)
-
-    def find_by_id(self, event_id: str) -> Optional[TrafficEvent]:
-        event = self.db.get(event_id)
-        if event is not None:
-            event = TrafficEvent(**event)
-        return event
-
-    def upsert_event(self, event: TrafficEvent):
-        self.db[event.event_id] = event.to_entity()
-        self._dump()
-
-
-class DynamoTrafficEventsDb(TrafficEventsDb, DynamoDb):
+class DynamoTrafficEventsDb(DynamoDb):
     PARTITION_KEY_NAME = 'event_type'
     PARTITION_KEY_VALUE = 'dpp'
     SORT_KEY_NAME = 'event_id'
